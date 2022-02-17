e81295c73477a954d64cf8fd325c37520186b88b
Sirin k <sirinibin2006@gmail.com>
2022-02-17 21:32:34 +0300

    Enhancements

---
+++ src/product/json.js
@@ -0,0 +1,110 @@
+import React, { useState, forwardRef, useImperativeHandle, useRef } from "react";
+import { Modal, Button } from 'react-bootstrap';
+import Cookies from "universal-cookie";
+import NumberFormat from "react-number-format";
+import Barcode from 'react-barcode';
+import QRCode from "react-qr-code";
+import html2canvas from 'html2canvas';
+
+const ProductJson = forwardRef((props, ref) => {
+
+    useImperativeHandle(ref, () => ({
+        open(content) {
+            jsonContent = content;
+            setJsonContent(jsonContent);
+            SetShow(true);
+
+        },
+
+    }));
+
+    let [jsonContent, setJsonContent] = useState([]);
+
+
+    let [model, setModel] = useState({});
+    const cookies = new Cookies();
+
+    const [show, SetShow] = useState(false);
+
+    function handleClose() {
+        SetShow(false);
+    };
+
+    function getProductRetailPrice(product) {
+        let store_id = cookies.get("store_id");
+        if (!store_id || !product.unit_prices) {
+            return "";
+        }
+        for (let i = 0; i < product.unit_prices.length; i++) {
+            if (product.unit_prices[i].store_id === store_id) {
+                return parseFloat(product.unit_prices[i].retail_unit_price + parseFloat(product.unit_prices[i].retail_unit_price * 0.15)).toFixed(2);
+            }
+        }
+        return "";
+    }
+
+    let [copied, setCopied] = useState(false);
+
+    function copy() {
+        /* Get the text field */
+        var copyText = document.getElementById("data");
+
+        //console.log("copyText :", copyText.innerText);
+        /* Select the text field */
+        //copyText.select();
+        //copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        navigator.clipboard.writeText(copyText.innerText);
+        copied = true;
+        setCopied(true);
+        /* Alert the copied text */
+        // alert("Copied the text: " + copyText.innerText);
+    }
+
+
+    return (<>
+        <Modal show={show} size="xl" onHide={handleClose} animation={false} scrollable={true}>
+            <Modal.Header>
+                <Modal.Title>Json </Modal.Title>
+
+                <div className="col align-self-end text-end">
+                    {/*
+                        <button
+                            className="btn btn-primary mb-3"
+                            data-bs-toggle="modal"
+                            data-bs-target="#previewProductModal"
+                        >
+                            <i className="bi bi-display"></i> Preview
+                        </button> */}
+                    <button
+                        type="button"
+                        className="btn-close"
+                        onClick={handleClose}
+                        aria-label="Close"
+                    ></button>
+
+                </div>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="row">
+                    <div className="col-md-12 align-self-end text-end">
+                        <Button variant="primary" onClick={copy} >
+                            {copied ? "Copied" : "Copy"}
+
+                        </Button>
+                    </div>
+                </div>
+                <code>
+                    <pre id="data">{JSON.stringify(jsonContent, null, 2)}</pre>
+
+                </code>
+
+            </Modal.Body>
+
+        </Modal>
+    </>);
+
+});
+
+export default ProductJson;
