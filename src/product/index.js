98ae174e244f8717511d42a3513b7c74e453cb11
Sirin k <sirinibin2006@gmail.com>
2022-02-18 03:07:45 +0300

    Enhancements

--- src/product/index.js
+++ src/product/index.js
@@ -456,6 +456,9 @@ function ProductIndex(props) {
                                                     <option value="40">40</option>
                                                     <option value="50">50</option>
                                                     <option value="100">100</option>
+                                                    <option value="200">200</option>
+                                                    <option value="300">300</option>
+                                                    <option value="500">500</option>
                                                 </select>
                                             </>
                                         )}
