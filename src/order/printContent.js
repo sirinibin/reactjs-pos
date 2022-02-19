63d2c6f5e8dcf79aa376e756dedac83e756b02a3
Sirin k <sirinibin2006@gmail.com>
2022-02-19 20:06:17 +0300

    Enhancements

--- src/order/printContent.js
+++ src/order/printContent.js
@@ -72,7 +72,7 @@ const OrderPrintContent = forwardRef((props, ref) => {
             <table style={{ fontSize: "4mm", position: "relative", left: "17px", top: "27px", border: "solid 0px", }}>
                 <tbody>
                     {props.model.products && props.model.products.map((product, index) => (
-                        <tr key={product.item_code} style={{ height: "28px", verticalAlign: "center", borderBottom: "solid 2px" }}>
+                        <tr key={product.item_code} style={{ height: "28px", verticalAlign: "center", borderBottom: "solid 1px" }}>
                             <th className="text-center" style={{ border: "solid 0px", width: "76px", }}>
                                 <h1 style={{ fontSize: "4mm" }}>{index + 1}
                                 </h1>
