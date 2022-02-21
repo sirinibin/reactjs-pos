c680628a778dabc31230080219f53a29bc65d4eb
Sirin k <sirinibin2006@gmail.com>
2022-02-21 21:09:40 +0300

    Enhancements

--- src/quotation/printContent.js
+++ src/quotation/printContent.js
@@ -48,7 +48,7 @@ const QuotationPrintContent = forwardRef((props, ref) => {
 
 
             >
-                <h2 style={{ fontSize: "4mm", position: "absolute", left: "305px", top: (35 + page.top) + "px", border: "solid " + border + "px", textDecoration: "underline", }}>
+                <h2 style={{ fontSize: "4mm", position: "absolute", left: "305px", top: (40 + page.top) + "px", border: "solid " + border + "px", textDecoration: "underline", }}>
                     {"QUOTATION / اقتباس"}
                 </h2>
 
