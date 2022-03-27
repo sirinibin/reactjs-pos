8d01e897dde4205764acc564135313a2024cf93c
Sirin k <sirinibin2006@gmail.com>
2022-03-27 06:09:00 +0300

    Adding product history

--- src/purchase/previewContent.js
+++ src/purchase/previewContent.js
@@ -42,7 +42,7 @@ const QuotationPreviewContent = forwardRef((props, ref) => {
                     border: "solid 2px",
                     borderColor: "silver",
                     borderRadius: "2mm",
-                    padding: "28px"
+                    padding: "32px"
                 }}
 
             >
