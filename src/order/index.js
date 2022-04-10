3a3a17c44e6b548496c0bcfaee3ed1535d7d61db
Sirin k <sirinibin2006@gmail.com>
2022-04-10 05:35:59 +0300

    Fix

--- src/order/index.js
+++ src/order/index.js
@@ -185,10 +185,18 @@ function OrderIndex(props) {
 
     function searchByDateField(field, value) {
         if (value) {
+            //value = "Sun Apr 10 2022 01:16:24 GMT+0300 (Arabian Standard Time)"
+            /*
+            console.log("value:", value);
             let d = new Date(value);
+            d = new Date(format(d, "yyyy-MM-dd"));
+            console.log("d1:", d);
+            d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
             d = new Date(d.toUTCString());
-
+            */
+            let d = new Date(value);
             value = format(d, "MMM dd yyyy");
+            console.log("value2:", value);
         } else {
             value = "";
         }
