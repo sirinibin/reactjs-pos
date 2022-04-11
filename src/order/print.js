63833484c3d67d19de0fff8f5fa15fa561c648f9
Sirin k <sirinibin2006@gmail.com>
2022-04-11 13:54:08 +0300

    Fix

--- src/order/print.js
+++ src/order/print.js
@@ -65,7 +65,7 @@ const OrderPrint = forwardRef((props, ref) => {
                         }
                     }
 
-                    top += 1062;
+                    top += 1064;
                     offset += pageSize;
                     if ((i + 1) === totalPages) {
                         model.pages[i].lastPage = true;
