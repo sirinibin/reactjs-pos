061614ad05453dd404182d99eb14abf13ef86160
Sirin k <sirinibin2006@gmail.com>
2022-04-26 15:37:37 +0300

    Fix

--- src/order/preview.js
+++ src/order/preview.js
@@ -29,7 +29,7 @@ const OrderPreview = forwardRef((props, ref) => {
                     getSignature(model.delivered_by_signature_id);
                 }
 
-                let pageSize = 40;
+                let pageSize = 35;
                 model.pageSize = pageSize;
                 let totalProducts = model.products.length;
                 let top = 0;
