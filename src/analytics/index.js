b4cf758538b86611dee6e1bfeae15fc5c0f7b97e
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2023-01-29 18:10:42 +0530

    Adding annocation Chart

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect, useRef, forwardRef } from "react";
 import Cookies from "universal-cookie";
-import AllSales from "./allSales";
-import HourlySales from "./hourlySales";
-import DailySales from "./dailySales";
-import MonthlySales from "./monthlySales";
-import YearlySales from "./yearlySales";
+import AllSales from "./sales/allSales";
+import HourlySales from "./sales/hourlySales";
+import DailySales from "./sales/dailySales";
+import MonthlySales from "./sales/monthlySales";
+import YearlySales from "./sales/yearlySales";
 
 
 
 const Analytics = forwardRef((props, ref) => {
     const cookies = new Cookies();
     let [allOrders, setAllOrders] = useState([]);
+    let [allExpenses, setAllExpenses] = useState([]);
+    let [allPurchases, setAllPurchases] = useState([]);
     const [searchParams, setSearchParams] = useState({});
     let [fettingAllRecordsInProgress, setFettingAllRecordsInProgress] = useState(false);
     let [sortField, setSortField] = useState("created_at");
@@ -26,7 +28,7 @@ const Analytics = forwardRef((props, ref) => {
     }
 
 
-    async function getAllOrders() {
+    async function getAllRecords(model, fields) {
         const requestOptions = {
             method: "GET",
             headers: {
@@ -34,8 +36,8 @@ const Analytics = forwardRef((props, ref) => {
                 Authorization: cookies.get("access_token"),
             },
         };
-        let Select =
-            "select=id,code,date,total,net_total,net_profit,loss,shipping_handling_fees,discount_percent,discount,products,customer_name,created_at,vat_price,customer_id,customer.id,customer.vat_no";
+        let Select = fields;
+
 
         if (cookies.get("store_id")) {
             searchParams.store_id = cookies.get("store_id");
@@ -52,19 +54,16 @@ const Analytics = forwardRef((props, ref) => {
             queryParams = "&" + queryParams;
         }
 
-        let size = 500;
+        let size = 1000;
 
-        let orders = [];
+        let records = [];
         var pageNo = 1;
 
-        //makeSalesReportFilename();
-
         for (; true;) {
-
             fettingAllRecordsInProgress = true;
             setFettingAllRecordsInProgress(true);
             let res = await fetch(
-                "/v1/order?" +
+                "/v1/" + model + "?" +
                 Select +
                 queryParams +
                 "&sort=" +
@@ -93,12 +92,7 @@ const Analytics = forwardRef((props, ref) => {
                         return [];
                     }
 
-
-                    // console.log("Orders:", orders);
-
                     return data.result;
-
-
                 })
                 .catch((error) => {
                     console.log(error);
@@ -109,30 +103,42 @@ const Analytics = forwardRef((props, ref) => {
             if (res.length === 0) {
                 break;
             }
-            orders = orders.concat(res);
+            records = records.concat(res);
             pageNo++;
         }
+        fettingAllRecordsInProgress = false;
+        setFettingAllRecordsInProgress(false);
+
+        return records;
+    }
+
+    useEffect(async () => {
+        let fields = "select=id,code,date,total,net_total,net_profit,loss,shipping_handling_fees,discount_percent,discount,products,customer_name,created_at,vat_price,customer_id,customer.id,customer.vat_no";
+        let orders = await getAllRecords("order", fields);
+        fields = "select=id,code,date,amount,description,category_name,created_by_name,created_at";
+        let expenses = await getAllRecords("expense", fields);
+
+        fields = "select=id,code,date,net_total,discount,vat_price,total,store_id,created_by_name,vendor_name,vendor_invoice_no,status,created_at,updated_at,net_retail_profit,net_wholesale_profit";
+        let purchases = await getAllRecords("purchase", fields);
+
 
         allOrders = orders;
         setAllOrders(orders);
-
         console.log("allOrders:", allOrders);
-        //prepareExcelData();
-        fettingAllRecordsInProgress = false;
-        setFettingAllRecordsInProgress(false);
+
+        allExpenses = expenses;
+        setAllExpenses(expenses);
+        console.log("allExpenses:", allExpenses);
+
+        allPurchases = purchases;
+        setAllPurchases(purchases);
+        console.log("allPurchases:", allPurchases);
+
         initAllSalesGraph();
         initHourlySalesGraph();
         initDailySalesGraph();
         initMonthlySalesGraph();
         initYearlySalesGraph();
-
-    }
-
-
-
-
-    useEffect(() => {
-        getAllOrders();
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -178,15 +184,15 @@ const Analytics = forwardRef((props, ref) => {
 
     return (
         <>
-            <AllSales ref={AllSalesRef} allOrders={allOrders} />
+            <AllSales ref={AllSalesRef} allOrders={allOrders} allExpenses={allExpenses} allPurchases={allPurchases} />
             <hr />
-            <HourlySales ref={HourlySalesRef} allOrders={allOrders} />
+            <HourlySales ref={HourlySalesRef} allOrders={allOrders} allExpenses={allExpenses} allPurchases={allPurchases} />
             <hr />
-            <DailySales ref={DailySalesRef} allOrders={allOrders} />
+            <DailySales ref={DailySalesRef} allOrders={allOrders} allExpenses={allExpenses} allPurchases={allPurchases} />
             <hr />
-            <MonthlySales ref={MonthlySalesRef} allOrders={allOrders} />
+            <MonthlySales ref={MonthlySalesRef} allOrders={allOrders} allExpenses={allExpenses} allPurchases={allPurchases} />
             <hr />
-            <YearlySales ref={YearlySalesRef} allOrders={allOrders} />
+            <YearlySales ref={YearlySalesRef} allOrders={allOrders} allExpenses={allExpenses} allPurchases={allPurchases} />
         </>
     );
 });
