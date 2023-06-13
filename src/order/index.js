9bf872bc3d3d2e2e5b9d602b8bccd58c6d65bd0d
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2023-06-13 08:16:22 +0530

    Adding build

--- src/order/index.js
+++ src/order/index.js
@@ -1015,7 +1015,7 @@ const OrderIndex = forwardRef((props, ref) => {
                 </div>
                 <div className="row">
                     <div className="col">
-                        <h1 className="h3">Sales Orders</h1>
+                        <h1 className="h3">Sales Orders123</h1>
                     </div>
 
 
