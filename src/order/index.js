acc35e829349056cbb8d2ce49cc32dda6d0f27f9
sirin kolasseri <sirinibin2006@gmail.com>
2024-12-27 16:11:45 +0530

    test

--- src/order/index.js
+++ src/order/index.js
@@ -1146,7 +1146,7 @@ const OrderIndex = forwardRef((props, ref) => {
                 </div>
                 <div className="row">
                     <div className="col">
-                        <h1 className="h3">Sales Orders123</h1>
+                        <h1 className="h3">Sales Orders123-123</h1>
                     </div>
 
 
