353c5a72ce64d55c35ac115f6bd4b0cb79a3017d
sirin kolasseri <sirinibin2006@gmail.com>
2025-02-15 01:32:13 +0300

    Adding zatca phase 2 connection

---
+++ src/store/zatca_connect.js
@@ -0,0 +1,216 @@
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from "react";
+import { Modal, Button } from "react-bootstrap";
+import Cookies from "universal-cookie";
+import { Spinner } from "react-bootstrap";
+
+const ZatcaConnect = forwardRef((props, ref) => {
+
+    useImperativeHandle(ref, () => ({
+        open(id) {
+            setErrors({});
+            selectedStores = [];
+            setSelectedStores(selectedStores);
+            formData = {};
+            if (id) {
+                formData.id = id;
+            }
+
+            setFormData(formData);
+
+            SetShow(true);
+        },
+
+    }));
+
+    useEffect(() => {
+        const listener = event => {
+            if (event.code === "Enter" || event.code === "NumpadEnter") {
+                console.log("Enter key was pressed. Run your function.");
+                // event.preventDefault();
+
+                var form = event.target.form;
+                if (form && event.target) {
+                    var index = Array.prototype.indexOf.call(form, event.target);
+                    if (form && form.elements[index + 1]) {
+                        if (event.target.getAttribute("class").includes("barcode")) {
+                            form.elements[index].focus();
+                        } else {
+                            form.elements[index + 1].focus();
+                        }
+                        event.preventDefault();
+                    }
+                }
+            }
+        };
+        document.addEventListener("keydown", listener);
+        return () => {
+            document.removeEventListener("keydown", listener);
+        };
+    }, []);
+
+
+    let [errors, setErrors] = useState({});
+    const [isProcessing, setProcessing] = useState(false);
+    const cookies = new Cookies();
+
+    //fields
+    let [formData, setFormData] = useState({});
+
+    const [show, SetShow] = useState(false);
+
+    function handleClose() {
+        SetShow(false);
+    }
+
+    useEffect(() => {
+        let at = cookies.get("access_token");
+        if (!at) {
+            window.location = "/";
+        }
+    });
+
+
+
+    function handleConnect(event) {
+        event.preventDefault();
+        console.log("Inside handle Connect");
+        let endPoint = "/v1/store/zatca/connect";
+        let method = "POST";
+
+        const requestOptions = {
+            method: method,
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+                Authorization: cookies.get("access_token"),
+            },
+            body: JSON.stringify(formData),
+        };
+
+        console.log("formData:", formData);
+
+        setProcessing(true);
+        fetch(endPoint, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers
+                    .get("content-type")
+                    ?.includes("application/json");
+                const data = isJson && (await response.json());
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = data && data.errors;
+                    //const error = data.errors
+                    return Promise.reject(error);
+                }
+
+                setErrors({});
+                setProcessing(false);
+
+                console.log("Response:");
+                console.log(data);
+                props.showToastMessage("Store Connected to Zatca Successfully!", "success");
+                if (props.refreshList) {
+                    props.refreshList();
+                }
+
+                handleClose();
+            })
+            .catch((error) => {
+                setProcessing(false);
+                console.log(error);
+                setErrors({ ...error });
+                console.error("There was an error!", error);
+                props.showToastMessage("Error Connecting to Zatca!", "danger");
+            });
+    }
+
+
+    let [selectedStores, setSelectedStores] = useState([]);
+
+
+    return (
+        <>
+            <Modal show={show} size="lg" onHide={handleClose} animation={false} backdrop="static" scrollable={true}>
+                <Modal.Header>
+                    <Modal.Title>
+                        {"Connect to Zatca"}
+                    </Modal.Title>
+
+                    <div className="col align-self-end text-end">
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={handleClose}
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                </Modal.Header>
+                <Modal.Body>
+                    <form className="row g-3 needs-validation" onSubmit={handleConnect}>
+
+
+                        <div className="col-md-6">
+                            <label className="form-label">OTP from Zatca*</label>
+
+                            <div className="input-group mb-3">
+                                <input
+                                    value={formData.otp ? formData.otp : ""}
+                                    type='string'
+                                    onChange={(e) => {
+                                        errors["code"] = "";
+                                        setErrors({ ...errors });
+                                        formData.otp = e.target.value;
+                                        setFormData({ ...formData });
+                                        console.log(formData);
+                                    }}
+                                    className="form-control"
+                                    id="otp"
+                                    placeholder="OTP"
+                                />
+
+
+
+                                <Button variant="primary" style={{ marginLeft: "8px" }} onClick={handleConnect} >
+                                    {isProcessing ? <Spinner
+                                        as="span"
+                                        animation="border"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden={true}
+                                    /> : "Connect"}
+                                </Button>
+
+                            </div>
+
+                            {errors.otp && (
+                                <div style={{ color: "red" }}>
+                                    {errors.otp}
+                                </div>
+                            )}
+                            {formData.otp && !errors.otp && (
+                                <div style={{ color: "green" }}>
+                                    <i className="bi bi-check-lg"> </i>
+                                    Looks good!
+                                </div>
+                            )}
+                        </div>
+
+
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </form>
+                </Modal.Body>
+
+            </Modal>
+
+
+        </>
+    );
+});
+
+export default ZatcaConnect;
