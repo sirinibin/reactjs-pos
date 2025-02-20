578574e999a17352d021a30ddcf3a6468b910fc9
sirin kolasseri <sirinibin2006@gmail.com>
2025-02-20 23:00:33 +0300

    Correcting units

--- src/product/create.js
+++ src/product/create.js
@@ -274,7 +274,7 @@ const ProductCreate = forwardRef((props, ref) => {
     console.log("productStores:", productStores);
   }
 
-  
+
 
   useEffect(() => {
     let at = cookies.get("access_token");
@@ -327,7 +327,7 @@ const ProductCreate = forwardRef((props, ref) => {
 
     formData.product_stores = storesData;
 
-    
+
     /*
     if (cookies.get("store_id")) {
       formData.store_id = cookies.get("store_id");
@@ -729,7 +729,6 @@ const ProductCreate = forwardRef((props, ref) => {
                 <option value="Meter(s)">Meter(s)</option>
                 <option value="Gm">Gm</option>
                 <option value="L">Liter (L)</option>
-                <option value="Gm">Gm</option>
                 <option value="Mg">Mg</option>
               </select>
             </div>
