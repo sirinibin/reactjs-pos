f7b04ed68567696b0c27bcbd9578c2d9e99b7ee7
sirin kolasseri <sirinibin2006@gmail.com>
2025-04-12 11:02:58 +0300

    Adding product brand

---
+++ src/product_brand/create.js
@@ -0,0 +1,309 @@
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from "react";
+import { Modal, Button } from "react-bootstrap";
+import Cookies from "universal-cookie";
+import { Spinner } from "react-bootstrap";
+
+
+
+const ProductBrandCreate = forwardRef((props, ref) => {
+
+    useImperativeHandle(ref, () => ({
+        open(id) {
+            formData = {};
+            setFormData(formData);
+
+            if (id) {
+                getProductBrand(id);
+            }
+            SetShow(true);
+        },
+
+    }));
+
+
+    useEffect(() => {
+        const listener = event => {
+            if (event.code === "Enter" || event.code === "NumpadEnter") {
+                console.log("Enter key was pressed. Run your function-product brand.");
+                // event.preventDefault();
+
+                var form = event.target.form;
+                if (form && event.target) {
+                    var index = Array.prototype.indexOf.call(form, event.target);
+                    if (form && form.elements[index + 1]) {
+                        if (event.target.getAttribute("class").includes("barcode")) {
+                            form.elements[index].focus();
+                        } else {
+                            form.elements[index + 1].focus();
+                        }
+                        event.preventDefault();
+                    }
+                }
+            }
+        };
+        document.addEventListener("keydown", listener);
+        return () => {
+            document.removeEventListener("keydown", listener);
+        };
+    }, []);
+
+    let [errors, setErrors] = useState({});
+    const [isProcessing, setProcessing] = useState(false);
+    const cookies = new Cookies();
+
+    //fields
+    let [formData, setFormData] = useState({});
+
+    const [show, SetShow] = useState(false);
+
+    function handleClose() {
+        SetShow(false);
+    }
+
+    useEffect(() => {
+        let at = cookies.get("access_token");
+        if (!at) {
+            window.location = "/";
+        }
+    });
+
+
+    function getProductBrand(id) {
+        console.log("inside get Product Brand");
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': cookies.get('access_token'),
+            },
+        };
+        formData = {};
+        setFormData({ ...formData });
+
+
+        let searchParams = {};
+        if (cookies.get("store_id")) {
+            searchParams.store_id = cookies.get("store_id");
+        }
+        let queryParams = ObjectToSearchQueryParams(searchParams);
+
+        fetch('/v1/product-brand/' + id + "?" + queryParams, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    const error = (data && data.errors);
+                    return Promise.reject(error);
+                }
+
+                setErrors({});
+
+                console.log("Response:");
+                console.log(data);
+
+                formData = data.result;
+                console.log("formData:", formData);
+
+
+                setFormData({ ...formData });
+                console.log("formData:", formData);
+
+            })
+            .catch(error => {
+                setProcessing(false);
+                setErrors(error);
+            });
+    }
+
+    function ObjectToSearchQueryParams(object) {
+        return Object.keys(object)
+            .map(function (key) {
+                return `search[${key}]=${object[key]}`;
+            })
+            .join("&");
+    }
+
+    function handleCreate(event) {
+        event.preventDefault();
+        console.log("Inside handle Create");
+
+
+        console.log("formData.logo:", formData.logo);
+
+
+        let endPoint = "/v1/product-brand";
+        let method = "POST";
+        if (formData.id) {
+            endPoint = "/v1/product-brand/" + formData.id;
+            method = "PUT";
+        } else if (cookies.get("store_id")) {
+            formData.store_id = cookies.get("store_id");
+        }
+
+
+        const requestOptions = {
+            method: method,
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+                Authorization: cookies.get("access_token"),
+            },
+            body: JSON.stringify(formData),
+        };
+
+        console.log("formData:", formData);
+
+        let searchParams = {};
+        if (cookies.get("store_id")) {
+            searchParams.store_id = cookies.get("store_id");
+        }
+        let queryParams = ObjectToSearchQueryParams(searchParams);
+
+        setProcessing(true);
+        fetch(endPoint + "?" + queryParams, requestOptions)
+            .then(async (response) => {
+                const isJson = response.headers
+                    .get("content-type")
+                    ?.includes("application/json");
+                const data = isJson && (await response.json());
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = data && data.errors;
+                    //const error = data.errors
+                    return Promise.reject(error);
+                }
+
+                setErrors({});
+                setProcessing(false);
+
+                console.log("Response:");
+                console.log(data);
+                props.showToastMessage("Product Brand Created Successfully!", "success");
+                if (props.refreshList) {
+                    props.refreshList();
+                }
+                handleClose();
+                props.openDetailsView(data.result.id);
+            })
+            .catch((error) => {
+                setProcessing(false);
+                console.log("Inside catch");
+                console.log(error);
+                setErrors({ ...error });
+                console.error("There was an error!", error);
+                props.showToastMessage("Error Creating ProductBrand!", "danger");
+            });
+    }
+
+
+
+
+
+    return (
+        <>
+            <Modal show={show} size="lg" onHide={handleClose} animation={false} backdrop="static" scrollable={true}>
+                <Modal.Header>
+                    <Modal.Title>
+                        {formData.id ? "Update Product Brand #" + formData.name : "Create New Product Brand"}
+                    </Modal.Title>
+
+                    <div className="col align-self-end text-end">
+                        {formData.id ? <Button variant="primary" onClick={() => {
+                            handleClose();
+                            props.openDetailsView(formData.id);
+                        }}>
+                            <i className="bi bi-eye"></i> View Detail
+                        </Button> : ""}
+                        &nbsp;&nbsp;
+                        <Button variant="primary" onClick={handleCreate} >
+                            {isProcessing ?
+                                <Spinner
+                                    as="span"
+                                    animation="border"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden={true}
+                                />
+
+                                : ""
+                            }
+                            {formData.id && !isProcessing ? "Update" : !isProcessing ? "Create" : ""}
+                        </Button>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={handleClose}
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                </Modal.Header>
+                <Modal.Body>
+                    <form className="row g-3 needs-validation" onSubmit={handleCreate}>
+
+                        <div className="col-md-6">
+                            <label className="form-label">Name*</label>
+
+                            <div className="input-group mb-3">
+                                <input
+                                    value={formData.name ? formData.name : ""}
+                                    type='string'
+                                    onChange={(e) => {
+                                        errors["name"] = "";
+                                        setErrors({ ...errors });
+                                        formData.name = e.target.value;
+                                        setFormData({ ...formData });
+                                        console.log(formData);
+                                    }}
+                                    className="form-control"
+                                    id="name"
+                                    placeholder="Name"
+                                />
+                                {errors.name && (
+                                    <div style={{ color: "red" }}>
+                                        <i className="bi bi-x-lg"> </i>
+                                        {errors.name}
+                                    </div>
+                                )}
+                                {formData.name && !errors.name && (
+                                    <div style={{ color: "green" }}>
+                                        <i className="bi bi-check-lg"> </i>
+                                        Looks good!
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+
+
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleCreate} >
+                                {isProcessing ?
+                                    <Spinner
+                                        as="span"
+                                        animation="bproductbrand"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden={true}
+                                    /> + " Processing..."
+
+                                    : formData.id ? "Update" : "Create"
+                                }
+                            </Button>
+                        </Modal.Footer>
+                    </form>
+                </Modal.Body>
+
+            </Modal>
+
+
+        </>
+    );
+});
+
+export default ProductBrandCreate;
---
+++ src/product_brand/view.js
@@ -0,0 +1,160 @@
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import { Modal, Table, Button } from 'react-bootstrap';
+import Cookies from "universal-cookie";
+
+const ProductBrandView = forwardRef((props, ref) => {
+
+    useImperativeHandle(ref, () => ({
+        open(id) {
+            if (id) {
+                getProductBrand(id);
+                SetShow(true);
+            }
+
+        },
+
+    }));
+
+
+    let [model, setModel] = useState({});
+    const cookies = new Cookies();
+
+    const [show, SetShow] = useState(false);
+
+    function handleClose() {
+        SetShow(false);
+    };
+
+    function ObjectToSearchQueryParams(object) {
+        return Object.keys(object)
+            .map(function (key) {
+                return `search[${key}]=${object[key]}`;
+            })
+            .join("&");
+    }
+
+    function getProductBrand(id) {
+        console.log("inside get ProductBrand");
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': cookies.get('access_token'),
+            },
+        };
+
+        let searchParams = {};
+        if (cookies.get("store_id")) {
+            searchParams.store_id = cookies.get("store_id");
+        }
+        let queryParams = ObjectToSearchQueryParams(searchParams);
+
+        fetch('/v1/product-brand/' + id + "?" + queryParams, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    const error = (data && data.errors);
+                    return Promise.reject(error);
+                }
+
+                console.log("Response:");
+                console.log(data);
+
+                model = data.result;
+
+                setModel({ ...model });
+            })
+            .catch(error => {
+                // setErrors(error);
+            });
+    }
+
+
+    return (<>
+        <Modal show={show} size="lg" onHide={handleClose} animation={false} scrollable={true}>
+            <Modal.Header>
+                <Modal.Title>Details of Product Brand #{model.name} </Modal.Title>
+
+                <div className="col align-self-end text-end">
+                    <Button variant="primary" onClick={() => {
+                        handleClose();
+                        props.openCreateForm();
+                    }}>
+                        <i className="bi bi-plus"></i> Create
+                    </Button>
+                    &nbsp;&nbsp;
+                    <Button variant="primary" onClick={() => {
+                        handleClose();
+                        props.openUpdateForm(model.id);
+                    }}>
+                        <i className="bi bi-pencil"></i> Edit
+                    </Button>
+                    <button
+                        type="button"
+                        className="btn-close"
+                        onClick={handleClose}
+                        aria-label="Close"
+                    ></button>
+
+                </div>
+            </Modal.Header>
+            <Modal.Body>
+                <Table striped bordered hover responsive="lg">
+                    <tbody>
+                        <tr>
+                            <th>Name:</th><td> {model.name}</td>
+                            <th>Parent Brand Name:</th><td> {model.parent_name}</td>
+                        </tr>
+                        <tr>
+                            <th>Created At:</th><td> {model.created_at}</td>
+                            <th>Updated At:</th><td> {model.updated_at}</td>
+                        </tr>
+                        <tr>
+                            <th>Created By:</th><td> {model.created_by_name}</td>
+                            <th>Updated By:</th><td> {model.updated_by_name}</td>
+                        </tr>
+                    </tbody>
+                </Table>
+
+                {/*
+                    <form className="row g-3 needs-validation" >
+                        
+                  
+                        <div className="col-md-6">
+                            <label className="form-label"
+                            >Delivered By*</label
+                            >
+
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" id="validationCustom06" placeholder="Select User" aria-label="Select User" aria-describedby="button-addon4" />
+                                <UserCreate showCreateButton={true} />
+                                <div className="valid-feedback">Looks good!</div>
+                                <div className="invalid-feedback">
+                                    Please provide a valid User.
+                  </div>
+                            </div>
+                        </div>
+                       
+
+                    </form>
+                    */}
+            </Modal.Body>
+            {/*
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                        Close
+                </Button>
+                    <Button variant="primary" onClick={this.handleClose}>
+                        Save Changes
+                </Button>
+                </Modal.Footer>
+                */}
+        </Modal>
+    </>);
+
+});
+
+export default ProductBrandView;
