34b0e87d565528b8b3f0dd90f045664fd6b9d3ef
sirin kolasseri <sirinibin2006@gmail.com>
2025-07-03 01:49:54 +0300

    Enhancements

--- src/order/create.js
+++ src/order/create.js
@@ -136,7 +136,8 @@ const OrderCreate = forwardRef((props, ref) => {
             formData.discount_with_vat = 0.00;
             formData.shipping_handling_fees = 0.00;
             formData.partial_payment_amount = 0.00;
-            formData.cash_discount = 0.00;
+            cashDiscount = "";
+            setCashDiscount(cashDiscount);
             formData.payment_method = "";
             formData.payment_status = "";
             formData.remarks = ""
@@ -153,7 +154,7 @@ const OrderCreate = forwardRef((props, ref) => {
                     "deleted": false,
                 }
             ];
-            formData.cash_discount = 0.00;
+
             ResetForm();
 
             if (id) {
@@ -243,6 +244,14 @@ const OrderCreate = forwardRef((props, ref) => {
 
                 formData.enable_report_to_zatca = false;
                 formData.date_str = data.result.date;
+
+                if (data.result?.cash_discount) {
+                    cashDiscount = data.result.cash_discount;
+                    setCashDiscount(cashDiscount);
+                } else {
+                    cashDiscount = "";
+                    setCashDiscount(cashDiscount);
+                }
 
                 if (data.result?.rounding_amount) {
                     roundingAmount = data.result.rounding_amount;
@@ -664,14 +673,16 @@ const OrderCreate = forwardRef((props, ref) => {
         event.preventDefault();
         let haveErrors = false;
 
-        if (!formData.cash_discount) {
-            formData.cash_discount = 0.00;
-        }
-
         if (!roundingAmount) {
             formData.rounding_amount = 0;
         } else {
             formData.rounding_amount = roundingAmount;
+        }
+
+        if (!cashDiscount) {
+            formData.cash_discount = 0;
+        } else {
+            formData.cash_discount = cashDiscount;
         }
 
         if (discount) {
@@ -1435,6 +1446,7 @@ const OrderCreate = forwardRef((props, ref) => {
     }
     */
 
+    let [cashDiscount, setCashDiscount] = useState("");
     let [roundingAmount, setRoundingAmount] = useState(0.00);
     let [shipping, setShipping] = useState(0.00);
     let [discount, setDiscount] = useState(0.00);
@@ -1446,6 +1458,12 @@ const OrderCreate = forwardRef((props, ref) => {
 
     async function reCalculate(productIndex) {
         console.log("inside reCalculate");
+
+        if (!cashDiscount) {
+            formData.cash_discount = 0;
+        } else {
+            formData.cash_discount = cashDiscount;
+        }
 
         if (!roundingAmount) {
             formData.rounding_amount = 0;
@@ -1620,17 +1638,6 @@ const OrderCreate = forwardRef((props, ref) => {
                     setDiscountPercentWithVAT(discountPercentWithVAT);
                 }
 
-                /*
-                if (res.result.discount) {
-                    discount = res.result.discount;
-                    setDiscount(discount);
-                }
-    
-                if (res.result.discount_with_vat) {
-                    discountWithVAT = res.result.discount_with_vat;
-                    setDiscountWithVAT(discountWithVAT);
-                }*/
-
 
                 if (res.result.shipping_handling_fees) {
                     formData.shipping_handling_fees = res.result.shipping_handling_fees;
@@ -1641,51 +1648,12 @@ const OrderCreate = forwardRef((props, ref) => {
                     for (let j = 0; j < res.result?.products?.length; j++) {
                         if (res.result?.products[j].product_id === selectedProducts[i].product_id) {
 
-                            /*
-                            if (res.result?.products[j].unit_discount_percent) {
-                                selectedProducts[i].unit_discount_percent = res.result?.products[j].unit_discount_percent;
-                            }
-    
-                            if (res.result?.products[j].unit_discount_percent_with_vat) {
-                                selectedProducts[i].unit_discount_percent_with_vat = res.result?.products[j].unit_discount_percent_with_vat;
-                            }
-    
-                            if (res.result?.products[j].unit_discount) {
-                                selectedProducts[i].unit_discount = res.result?.products[j].unit_discount;
-                            }
-                                
-                            if (res.result?.products[j].unit_price) {
-                                selectedProducts[i].unit_price = res.result?.products[j].unit_price;
-                            }
-    
-                            if (res.result?.products[j].unit_price_with_vat) {
-                                selectedProducts[i].unit_price_with_vat = res.result?.products[j].unit_price_with_vat;
-                            }
-                            */
 
-                            /*
-                            if (res.result?.products[j].unit_price) {
-                                selectedProducts[i].unit_price = res.result?.products[j].unit_price;
-                            } else if (res.result?.products[j].unit_price === 0 || !res.result?.products[j].unit_price) {
-                                selectedProducts[i].unit_price = "";
-                            }
-     
-                            if (res.result?.products[j].unit_price_with_vat) {
-                                selectedProducts[i].unit_price_with_vat = res.result?.products[j].unit_price_with_vat;
-                            } else if (res.result?.products[j].unit_price_with_vat === 0 || !res.result?.products[j].unit_price_with_vat) {
-                                selectedProducts[i].unit_price_with_vat = "";
-                            }
-                                */
                             console.log("Discounts updated from server")
                         }
                     }
                 }
-                //   setSelectedProducts([...selectedProducts]);
-                /*
-                    selectedProducts[index].unit_discount_percent
-                    selectedProducts = formData.products;
-                    setSelectedProducts([...selectedProducts]);
-                */
+
                 setFormData({ ...formData });
             }
 
@@ -1706,8 +1674,8 @@ const OrderCreate = forwardRef((props, ref) => {
 
                     if (formData.net_total > 0) {
                         formData.payments_input[0].amount = parseFloat(trimTo2Decimals(formData.net_total));
-                        if (formData.cash_discount) {
-                            formData.payments_input[0].amount = formData.payments_input[0].amount - parseFloat(trimTo2Decimals(formData.cash_discount));
+                        if (cashDiscount) {
+                            formData.payments_input[0].amount = formData.payments_input[0].amount - parseFloat(trimTo2Decimals(cashDiscount));
                         }
                         formData.payments_input[0].amount = parseFloat(trimTo2Decimals(formData.payments_input[0].amount));
                     }
@@ -1736,11 +1704,11 @@ const OrderCreate = forwardRef((props, ref) => {
         console.log("totalPaymentAmount:", totalPaymentAmount);
         setTotalPaymentAmount(totalPaymentAmount);
         console.log("totalPayment:", totalPayment)
-        balanceAmount = (parseFloat(trimTo2Decimals(formData.net_total)) - parseFloat(parseFloat(trimTo2Decimals(formData.cash_discount)))) - parseFloat(trimTo2Decimals(totalPayment));
+        balanceAmount = (parseFloat(trimTo2Decimals(formData.net_total)) - parseFloat(parseFloat(trimTo2Decimals(cashDiscount)))) - parseFloat(trimTo2Decimals(totalPayment));
         balanceAmount = parseFloat(trimTo2Decimals(balanceAmount));
         setBalanceAmount(balanceAmount);
 
-        if (balanceAmount === parseFloat((parseFloat(trimTo2Decimals(formData.net_total)) - parseFloat(trimTo2Decimals(formData.cash_discount))))) {
+        if (balanceAmount === parseFloat((parseFloat(trimTo2Decimals(formData.net_total)) - parseFloat(trimTo2Decimals(cashDiscount))))) {
             paymentStatus = "not_paid"
         } else if (balanceAmount <= 0) {
             paymentStatus = "paid"
@@ -1838,7 +1806,7 @@ const OrderCreate = forwardRef((props, ref) => {
         }
 
 
-        if (formData.cash_discount > 0 && formData.cash_discount >= formData.net_total) {
+        if (cashDiscount > 0 && cashDiscount >= formData.net_total) {
             errors["cash_discount"] = "Cash discount should not be >= " + trimTo2Decimals(formData.net_total).toString();
             setErrors({ ...errors });
             haveErrors = true
@@ -2131,6 +2099,7 @@ const OrderCreate = forwardRef((props, ref) => {
     );
 
     const inputRefs = useRef({});
+    const cashDiscountRef = useRef(null);
     /*
     const handleFocus = (rowIdx, field) => {
         const ref = inputRefs.current?.[rowIdx]?.[field];
@@ -4734,26 +4703,65 @@ const OrderCreate = forwardRef((props, ref) => {
                                 */}
                         <div className="col-md-2">
                             <label className="form-label">Cash discount</label>
-                            <input type='number' id="sales_cash_discount" name="sales_cash_discount" value={formData.cash_discount} className="form-control "
+                            <input
+                                type='number'
+                                ref={cashDiscountRef}
+                                id="sales_cash_discount"
+                                name="sales_cash_discount"
+                                value={cashDiscount}
+                                className="form-control"
                                 onChange={(e) => {
                                     delete errors["cash_discount"];
                                     setErrors({ ...errors });
                                     if (!e.target.value) {
-                                        formData.cash_discount = e.target.value;
-                                        setFormData({ ...formData });
-                                        validatePaymentAmounts();
+                                        cashDiscount = e.target.value;
+                                        setCashDiscount(cashDiscount);
+
+                                        if (timerRef.current) clearTimeout(timerRef.current);
+                                        timerRef.current = setTimeout(() => {
+                                            // validatePaymentAmounts();
+                                            reCalculate();
+                                        }, 100);
+
                                         return;
                                     }
-                                    formData.cash_discount = parseFloat(e.target.value);
-                                    if (formData.cash_discount > 0 && formData.cash_discount >= formData.net_total) {
-                                        errors["cash_discount"] = "Cash discount should not be >= " + formData.net_total.toString();
+
+                                    cashDiscount = parseFloat(e.target.value);
+                                    setCashDiscount(cashDiscount);
+
+                                    if (cashDiscount > 0 && cashDiscount >= formData.net_total) {
+                                        errors["cash_discount"] = "Cash discount should not be greater than or equal to Net Total: " + formData.net_total?.toString();
                                         setErrors({ ...errors });
                                         return;
                                     }
 
-                                    setFormData({ ...formData });
-                                    validatePaymentAmounts();
+                                    if (timerRef.current) clearTimeout(timerRef.current);
+                                    timerRef.current = setTimeout(() => {
+                                        //  validatePaymentAmounts();
+                                        reCalculate();
+                                    }, 100);
                                     console.log(formData);
+                                }}
+
+                                onKeyDown={(e) => {
+                                    if (timerRef.current) clearTimeout(timerRef.current);
+
+                                    if (e.key === "Backspace") {
+                                        cashDiscount = "";
+                                        setCashDiscount(cashDiscount);
+
+                                        if (timerRef.current) clearTimeout(timerRef.current);
+                                        timerRef.current = setTimeout(() => {
+                                            reCalculate();
+                                        }, 100);
+                                        return;
+                                    }
+                                }}
+                                onFocus={() => {
+                                    if (timerRef.current) clearTimeout(timerRef.current);
+                                    timerRef.current = setTimeout(() => {
+                                        cashDiscountRef.current.select();
+                                    }, 20);
                                 }}
                             />
                             {errors.cash_discount && (
