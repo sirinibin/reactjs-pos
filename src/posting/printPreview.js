a6c169d343c600d1a8c1f46efb70efa3ade8df71
sirin kolasseri <sirinibin2006@gmail.com>
2025-09-07 12:08:05 +0300

    Adding more page sizes to the balance sheet

--- src/posting/printPreview.js
+++ src/posting/printPreview.js
@@ -982,8 +982,20 @@ const handlePrint = useCallback(async () => {
                             <option value="23">23</option>
                             <option value="24">24</option>
                             <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
                             <option value="30">30</option>
+                            <option value="31">31</option>
+                            <option value="32">32</option>
+                            <option value="33">33</option>
+                            <option value="34">34</option>
                             <option value="35">35</option>
+                            <option value="36">36</option>
+                            <option value="37">37</option>
+                            <option value="38">38</option>
+                            <option value="39">39</option>
                             <option value="40">40</option>
                         </select>
                     </>
