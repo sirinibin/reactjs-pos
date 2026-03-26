25467433dc80b9839be58f7f7c097133cfa39cf3
sirin kolasseri <sirinibin2006@gmail.com>
2026-03-26 18:51:58 +0300

    Adding vendor namme in arabic to invoice

--- src/order/previewContentWithSellerInfo.js
+++ src/order/previewContentWithSellerInfo.js
@@ -615,6 +615,7 @@ const PreviewContentWithSellerInfo = forwardRef((props, ref) => {
                                             {props.model.vendor ? props.model.vendor.name : ""}
                                             {!props.model.vendor && props.model.vendorName ? props.model.vendorName : ""}
                                             {!props.model.vendorName && !props.model.vendor ? "N/A" : ""}
+                                            {props.model.vendor?.name_in_arabic ? "  " + props.model.vendor.name_in_arabic : ""}
                                         </div>
                                     </div>
 
